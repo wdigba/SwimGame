2c19b938842436cfbfc19f15eeea0a2ebf9225ae
Karina <karina93209@gmail.com>
2023-04-20 23:55:21 +0200

    added methods to Card and one method to Swim

--- src/main/kotlin/entity/Card.kt
+++ src/main/kotlin/entity/Card.kt
@@ -7,13 +7,20 @@ data class Card (val suit: CardSuit,
                  val value: CardValue,
                  private val random : Random = Random
 ){
-    private val cards: ArrayDeque<Card> = ArrayDeque(32) //stack of all cards
+    val cards: ArrayDeque<Card> = ArrayDeque(32) //stack of all cards
 
     val size: Int get() = cards.size //amount of cards in stack
     val empty: Boolean get() = cards.isEmpty() //check if there are enough cards in stack
     fun shuffle() { //shuffle cards in stack
         cards.shuffle(random)
     }
-
+    fun draw(amount: Int = 1): List<Card> //draws required amount of cards from the stack
+    {
+        require (amount in 1..cards.size) { "can't draw $amount cards from $cards" }
+        return List(amount) { cards.removeFirst() }
+    }
+    fun drawAll(): List<Card> = draw(size) // draws cards from stack
+    fun drawOne(): List<Card> = draw(1) // draws exactly one card from stack
+    fun drawThree(): List<Card> = draw(3) // draws exactly three cards from stack
     override fun toString() ="$suit$value"
 }
--- src/main/kotlin/entity/Swim.kt
+++ src/main/kotlin/entity/Swim.kt
@@ -1,12 +1,18 @@
 package entity
 
 class Swim (var numberOfPasses : Int,
-    var remainingTurns : Int,
-    var lastRound: Boolean,
-    var actPlayer: Player,
-    var playerList: List<Player> = emptyList(),
-    var midCards: List<Card> = emptyList(),
-    var deckCards: List<Card> = emptyList()
+            var remainingTurns : Int,
+            var lastRound: Boolean,
+            var actPlayer: Player
     ) {
+    var playerList: MutableList<Player> = mutableListOf()
+    var midCards: MutableList<Card> = mutableListOf()
+    var deckCards: MutableList<Card> = mutableListOf()
+
+    fun addPlayer(newPlayer : Player) {
+        if (playerList.size>4) throw IllegalArgumentException ("Too much players in this game")
+        playerList.add(newPlayer)
+    }
+
 
 }
