c4f76ffdcea7bf02e8d99cbb21cf085c0f1abb8f
Karina <karina93209@gmail.com>
2023-04-20 13:50:15 +0200

    added new methods to class Player

--- src/main/kotlin/entity/Player.kt
+++ src/main/kotlin/entity/Player.kt
@@ -2,8 +2,24 @@ package entity
 
 class Player (val playerName: String,
               var points : Double = 0.0,
-              handCards:List<Card> = emptyList()
+              var handCards:MutableList<Card> = mutableListOf()
     ){
-
-
+    fun addCards(cardStack: MutableList<Card>) { // add cards from stack to player´s hand
+        if (cardStack.size<3) throw IllegalArgumentException("Not enough cards in the stack")
+        for (i in 1..3) {
+            val newCard = cardStack.removeAt(0) //removes card from cardsStack from index 0 and returns it
+            handCards.add(newCard)
+        }
+    }
+    fun removeCards(cardStack: MutableList<Card>) { //remove cards from player´s hand
+        if (cardStack.size<3) throw IllegalArgumentException ("Not enough cards in the stack")
+        handCards.clear()
+        for (i in 1..3) {
+            val newCard = cardStack.removeAt(0)
+            handCards.add(newCard)
+        }
+    }
+    fun updatePoints (newPoints: Double) {
+        points+=newPoints
+    }
 }
