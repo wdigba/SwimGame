39f883890a306570c18bd72fa6d0be1030013c0e
Karina <karina93209@gmail.com>
2023-04-23 15:09:58 +0200

    moved methods from Card to Swim because of logical construction (all operations are happening in Game, not in Card)

--- src/main/kotlin/entity/Card.kt
+++ src/main/kotlin/entity/Card.kt
@@ -1,11 +1,7 @@
 package entity
 
-import kotlin.random.Random
-/*Data structure for all Cards in game, provides stack-like construction
-* */
 data class Card (val suit: CardSuit,
                  val value: CardValue,
-    ) {
-
+    ){
     override fun toString() ="$suit$value"
 }
--- src/main/kotlin/entity/Player.kt
+++ src/main/kotlin/entity/Player.kt
@@ -1,21 +1,7 @@
 package entity
 
-class Player (val playerName: String,
+data class Player (val playerName: String,
+              var handCards:MutableList<Card>,
               var points : Double = 0.0,
-              var handCards:MutableList<Card> = mutableListOf()
     ){
-    fun addCards(cardStack: MutableList<Card>) { // add cards from stack to player´s hand
-        if (cardStack.size<3) throw IllegalArgumentException("Not enough cards in the stack")
-        for (i in 1..3) {
-            val newCard = cardStack.removeAt(0) //removes card from cardsStack from index 0 and returns it
-            handCards.add(newCard)
-        }
-    }
-    fun removeCards() { //remove cards from player´s hand
-        handCards.clear()
-
-    }
-    fun updatePoints (newPoints: Double) {
-        points+=newPoints
-    }
 }
