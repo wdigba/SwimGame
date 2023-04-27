9717c3736344ce3e435bee3d58bd0322db45740f
Karina <karina93209@gmail.com>
2023-04-28 00:46:40 +0200

    service classes supplemented

--- src/main/kotlin/service/GameService.kt
+++ src/main/kotlin/service/GameService.kt
@@ -1,19 +1,21 @@
 package service
 import entity.*
+
 class GameService (private val rootService: RootService) {
     fun startNewGame(playerList: MutableList<Player>,
                      allCards: MutableList<Card> = defaultRandomCardList()
     ) {
+        check(rootService.currentGame == null) {"Game already exist"}
         check (playerList.size in 2..4) {"Amount of players is incorrect"}
         allCards.shuffled()
         val game = Swim (
-            0,
-            4,
-            false,
-            playerList.first(),
-            playerList,
-            allCards.subList(0,3),
-            allCards.subList(4,32)
+            numberOfPasses = 0,
+            remainingTurns = 4,
+            lastRound = false,
+            actPlayer = playerList.first(),
+            playerList = playerList,
+            midCards = allCards.subList(0,3),
+            deckCards = allCards.subList(4,32)
         )
         rootService.currentGame = game
     }
@@ -32,14 +34,65 @@ class GameService (private val rootService: RootService) {
         rootService.currentGame = game
     }
     fun calculateWinner() {
+        val game = checkNotNull(rootService.currentGame) {"Current game does not exist"}
+        if (!isGameEnded()) {
+            println("Game has not been finished yet")
+            return
+        }
 
+        val playerScores = mutableMapOf<Player, Float>()
+        for (player in game.playerList) {
+            var score = 0.0f
+            var suitScore = 0.0f
+            for (suit in CardSuit.values()) {
+                val suitCards = player.handCards.filter { it.suit == suit }
+                if (suitCards.size == 3){
+                    suitScore = 30.5f
+                    break
+                }
+                else if (suitCards.isNotEmpty()) {
+                    for (card in suitCards) {
+                        suitScore += when (card.value) {
+                            CardValue.ACE -> 11.0f
+                            CardValue.TEN, CardValue.JACK, CardValue.QUEEN, CardValue.KING -> 10.0f
+                            CardValue.NINE -> 9.0f
+                            CardValue.EIGHT -> 8.0f
+                            CardValue.SEVEN -> 7.0f
+                            else -> throw IllegalArgumentException("Cards in stack start from 7")
+                        }
+                    }
+                }
+                check (suitScore <= 31.0) {"Amount of points is incorrect"}
+                score += suitScore
+            }
+            playerScores[player] = score
+        }
+        val winner = playerScores.maxByOrNull { it.value }!!
+        println("The winner is ${winner.key.playerName} with ${winner.value} points.")
+        //realisation is not fully correct, it does not count points correctly, in case all suits are different it gives sum of these cards back,
+        // but it has to give value of the biggest card instead
     }
     private fun isGameEnded() : Boolean {
         val game = checkNotNull(rootService.currentGame) {"Current game does not exist"}
         return (game.deckCards.isEmpty() || game.remainingTurns==0)
     }
-    /* fun´s frames:
-    changeMidCards (when numberofPasses == 4)
+    fun changeMidCards() {
+        val game = checkNotNull(rootService.currentGame) { "Current game does not exist" }
+        if (game.numberOfPasses < game.playerList.size) {
+            println("Cannot change mid cards yet, ${4 - game.numberOfPasses} pass(es) left")
+            return
+        }
+        if (game.deckCards.size < 3) {
+            println("Not enough cards in deck to change mid cards")
+            return
+            //end game??
+        }
+        game.midCards = game.deckCards.take(3).toMutableList()
+        game.deckCards = game.deckCards.drop(3).toMutableList()
+        game.numberOfPasses = 0
+        rootService.currentGame = game
+    }
+    /*
     realisation of lastRound? any consequences
     * */
 }
--- src/main/kotlin/service/PlayerActionService.kt
+++ src/main/kotlin/service/PlayerActionService.kt
@@ -1,4 +1,50 @@
 package service
+import entity.*
 
 class PlayerActionService (private val rootService: RootService) {
+    fun switchOneCard(midCard: Card, playerCard: Card){
+        val game = checkNotNull(rootService.currentGame) { "Current game does not exist" }
+        val currentPlayer = game.actPlayer
+        //checks if card exists in the tableCards or in the playerCards
+        if(midCard !in game.midCards || playerCard !in currentPlayer.handCards){
+            throw IllegalStateException("The specified card does not exist!")
+        }
+
+        val midCardIndex = game.midCards.indexOf(midCard)
+        val playerCardIndex = currentPlayer.handCards.indexOf(playerCard)
+
+        game.midCards[midCardIndex] = playerCard
+        currentPlayer.handCards[playerCardIndex] = midCard
+
+        game.numberOfPasses = 0
+        rootService.gameService.changeToNextPlayer()
+    }
+    fun switchAllCards(){
+        val game = checkNotNull(rootService.currentGame) { "Current game does not exist" }
+        val currentPlayer = game.actPlayer
+        val temporaryList = mutableListOf<Card>()
+        temporaryList.addAll(currentPlayer.handCards)
+        currentPlayer.handCards.clear()
+        currentPlayer.handCards.addAll(game.midCards)
+        game.midCards.clear()
+        game.midCards.addAll(temporaryList)
+
+        game.numberOfPasses = 0
+        rootService.gameService.changeToNextPlayer()
+    }
+    fun pass() {
+        val game = checkNotNull(rootService.currentGame) { "Current game does not exist" }
+        game.numberOfPasses++
+        if(game.numberOfPasses < game.playerList.size) {
+            rootService.gameService.changeToNextPlayer()
+        }
+        else rootService.gameService.changeMidCards()
+    }
+    fun knock () {
+        val game = checkNotNull(rootService.currentGame) { "Current game does not exist" }
+        //how to check if someone has already knocked? somehow we have to remember which player has knocked, so we could give turn to next
+        //players until that one person
+        rootService.gameService.changeToNextPlayer()
+    }
+
 }
